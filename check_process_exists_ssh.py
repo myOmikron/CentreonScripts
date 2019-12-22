c38d153e2098ef63e450aeb40a1b40afc2b57f1b
Niklas Pfister <accounts@omikron.pw>
2019-12-22 04:58:55 +0100

    Removed paramiko lib, changed lib to popen

--- check_process_exists_ssh.py
+++ check_process_exists_ssh.py
@@ -2,12 +2,10 @@
 
 import argparse
 import sys
-import io
-import paramiko
+from subprocess import PIPE, Popen
 
+if __name__ == "__main__":
 
-if __name__  == "__main__":
-    
     parser = argparse.ArgumentParser()
     parser.add_argument("-H", "--hostname", action="store", dest="host_name", help="Name of the host to connect to")
     parser.add_argument("-u", "--username", action="store", dest="user_name", help="Name of the user to connect with")
@@ -15,23 +13,13 @@ if __name__  == "__main__":
     parser.add_argument("-P", "--process", action="store", dest="process", help="Process to search")
     args = parser.parse_args()
 
-    try:
-        client = paramiko.client.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
-        
-        with open("/var/lib/centreon-engine/.ssh/id_rsa") as fh:
-            string = fh.read()
-            keyfile = io.StringIO(string)
-            read_private_key = paramiko.RSAKey.from_private_key(keyfile)
-
-        client.connect(args.host_name, username=args.user_name, pkey=read_private_key)
-        client.exec_command("ps -aux | grep " + args.process)
-    except paramiko.AuthenticationException:
-        print("SERVICE STATUS: Critical - Authentication error")
+    cmd = "pgrep " + args.process
+    stream = Popen(['ssh', args.user_name + "@" + args.host_name, "-p " + args.port if args.port else "-p 22", cmd],
+                   stdout=PIPE)
+    out = stream.stdout.read().decode('utf-8')
+    if len(out.splitlines()) > 0:
+        print("SERVICE STATUS: OK - Process is running")
+        sys.exit(0)
+    else:
+        print("SERVICE STATUS: CRITICAL - Process not found")
         sys.exit(2)
-    except:
-        print("SERVICE STATUS: Critical - Unkown Error")
-        sys.exit(2)
-    client.close()
-
