1e3e7d5cfc4a37b20a04ac0784c2d4ceda2e2f00
Niklas Pfister <accounts@omikron.pw>
2019-12-23 14:22:05 +0100

    Added ssh-option to specify options like proxy, etc.

--- check_ssh.py
+++ check_ssh.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 
-from paramiko.client import SSHClient
 import argparse
+import subprocess
 import sys
+import re
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-H", "--hostname", action="store", dest="host_name", help="Name of the host to connect to")
-    parser.add_argument("-u", "--user", action="store", dest="user_name", help="Name of the user")
-    parser.add_argument("-p", "--port", action="store", dest="port", help="Port of the SSH Server")
+    parser.add_argument("-H", "--hostname", action="store", required=True, dest="host_name", help="Name of the host to connect to")
+    parser.add_argument("--ssh-option", action="append", nargs="*", dest="ssh_options", help="SSH options to specify behaviour")
     args = parser.parse_args()
 
-    with SSHClient() as client:
-        client.load_system_host_keys()
-        try:
-            client.connect(args.host_name, port=args.port, username=args.user_name, key_filename="/var/lib/centreon-engine/.ssh/id_rsa")
-        except:
-            print("SERVICE STATUS: CRITICAL - SSH Server not reachable")
-            sys.exit(2)
-    print("SERVICE STATUS: OK - SSH Server responded correctly")
-    sys.exit(0)
+    run_list = ["ssh", args.host_name]
+    if args.ssh_options:
+        for item in args.ssh_options:
+            option = item[0]
+            if option.startswith("'"):
+                option = option[1:]
+            if option.endswith("'"):
+                option = option[:-1]
+            options = re.split("=", option)
+            for opt in options:
+                run_list.append(opt)
+
+    try:
+        ret = subprocess.run(run_list)           
+        if ret.returncode == 0:
+            print("SERVICE STATUS: OK - SSH Login successful")
+    except:
+        print("SERVICE STATUS: CRITICAL - SSH Server not reachable")
+        sys.exit(2)
+    print("SERVICE STATUS: UNKNOWN")
+    sys.exit(3)
