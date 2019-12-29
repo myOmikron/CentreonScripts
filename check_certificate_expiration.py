81ca72a542597f3717edaa28faaf2982723da0a1
Niklas Pfister <accounts@omikron.pw>
2019-12-30 00:58:21 +0100

    Added script to check the expiring date of ssl certificates

---
+++ check_certificate_expiration.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import OpenSSL
+import ssl, socket
+import dateutil.parser
+from datetime import datetime, timedelta
+import enum
+import sys
+import argparse
+import subprocess
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", action="store", required=True, dest="url", help="URL of the specified website")
+    parser.add_argument("-w", "--warning", action="store", required=True, type=int,  dest="warning", help="Days after which the script should return a warning")
+    parser.add_argument("-c", "--critical", action="store", required=True, type=int, dest="critical", help="Days after which the script should return a critical error")
+    parser.add_argument("-p", "--port", action="store", dest="port", help="Optional: Port from https server")
+    args = parser.parse_args()
+
+    if args.port is None:
+        args.port = 443
+    
+    cmd = "echo | openssl s_client -showcerts -servername " + args.url + " -connect " + args.url + ":" + args.port + " 2>/dev/null | openssl x509 -inform pem -enddate -noout"
+    ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    expiring_date = ret.stdout.read().decode('utf-8').split("=")[1][:-1]
+    expiring_date = dateutil.parser.parse(expiring_date)
+    expiring_date = expiring_date.replace(tzinfo=None)
+    now = datetime.now().replace(tzinfo=None)
+    remaining = expiring_date - now
+
+    if remaining < timedelta(days=args.critical):
+        print("SERVICE STATUS: Critical - Certificate expires at:", str(expiring_date.strftime("%b %d %Y %H:%M:%S")))
+        sys.exit(1)
+    elif remaining < timedelta(days=args.warning):
+        print("SERVICE STATUS: Warning - Certificate expires at:", str(expiring_date.strftime("%b %d %Y %H:%M:%S")))
+        sys.exit(2)
+    print("SERVICE STATUS: OK - Certificate expires at:", str(expiring_date.strftime("%b %d %Y %H:%M:%S")))
+    sys.exit(0)
+
+
+    
