7fed45be3af2cc1004ff789019dd94ffb60cdfbf
myOmikron <accounts@omikron.pw>
2020-02-14 16:28:15 +0100

    Refactored host notification handler. Now, it gives only relevant information and doesn't depend von telegram-bot-sdk

--- telegram_host_event_handler.py
+++ telegram_host_event_handler.py
@@ -1,46 +1,49 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 
-from telegram_bot_sdk.telegram import TelegramBot
 import argparse
+import requests
 
 
-class CentreonNotificationBot:
-    def __init__(self, credentials):
-        self.telebot = TelegramBot(credentials)
-        with open("/usr/lib64/nagios/plugins/CentreonScripts/allowed_ids") as fh:
-            self.allowed_ids = fh.readlines()
-
-    def send_message(self, message):
-        for chat_id in self.allowed_ids:
-            if len(chat_id) > 0:
-                if chat_id.endswith("\n"):
-                    self.telebot.send_message(chat_id=chat_id[:-1], text=message, disable_notification=False,
-                                              parse_mode="Markdown")
-                else:
-                    self.telebot.send_message(chat_id=chat_id, text=message, disable_notification=False,
-                                              parse_mode="Markdown")
-
-
-
-def main(arguments):
-    with open("/usr/lib64/nagios/plugins/CentreonScripts/credentials") as fh:
+def send_message(message, receiver):
+    with open('credentials') as fh:
         token = fh.readline()
-        if token.endswith("\n"):
+        if token.endswith('\n'):
             token = token[:-1]
-        bot = CentreonNotificationBot(token)
-    message = "*ALERT*\n\nType: {} \nState: {}\nHost: {}\nAddress: {}\nInfo: {}\nDate/Time: {}"
-    message = message.format(arguments.type, arguments.state, arguments.name, arguments.address, arguments.output,
-                             arguments.date)
-    bot.send_message(message)
+    param_dict = {"text": message,
+                  "chat_id": receiver,
+                  "parse_mode": "HTML"}
+    requests.get('https://api.telegram.org/bot' + token + '/sendMessage', data=param_dict)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', required=True, action='store', dest='name', help='Name of the host')
+    parser.add_argument('-s', '--state', required=True, action='store', dest='state', help='State of the host')
+    parser.add_argument('-a', '--address', required=True, action='store', dest='address', help='Address of the host')
+    parser.add_argument('-o', '--output', required=True, action='store', dest='output', help='Output of the host')
+    parser.add_argument('-i', '--id', required=True, action='store', dest='id', help='ID of the chat')
+    args = parser.parse_args()
+
+    if args.state == 'UP':
+        emoticon = '&#x2714;'
+    elif args.state == 'DOWN':
+        emoticon = '&#x1f525;'
+    elif args.state == 'RECOVERY':
+        emoticon = '&#x2934;;'
+    elif args.state == 'UNREACHABLE':
+        emoticon = '&#x2754;'
+    else:
+        emoticon = ''
+
+    message = '<b>HOST ' + args.state + ' ALERT</b> ' + emoticon + '\n\n'
+    message += 'Host: ' + args.name + '\n'
+    message += 'Address: ' + args.address + '\n'
+    message += 'Output: ' + args.output + '\n'
+    message += 'URL: https://centreon.omikron.dev/centreon/main.php?p=20202&o=hd&host_name=' + args.name
+    send_message(message, args.id)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--type", action="store", dest="type", help="Type of the notification")
-    parser.add_argument("-n", "--name",  action="store", dest="name", help="Name of the host")
-    parser.add_argument("-s", "--state",  action="store", dest="state", help="State of the host")
-    parser.add_argument("-a", "--address",  action="store", dest="address", help="Address of the host")
-    parser.add_argument("-o", "--output",  action="store", dest="output", help="Output of the host")
-    parser.add_argument("-d", "--date",  action="store", dest="date", help="Date")
-    args = parser.parse_args()
-    main(args)
+    main()
+
